8dc3445987863720d10df8a0948812066b877013
dewanshkaushik <kaushik.dewansh1@gmail.com>
2024-02-04 16:25:23 +0530

    commit message goes here

--- src/main/java/com/springrest/springrest/entity/Courses.java
+++ src/main/java/com/springrest/springrest/entity/Courses.java
@@ -1,7 +1,7 @@
 package com.springrest.springrest.entity;
 
 public class Courses {
-	private long id;
+	private long idd;
 	private String title;
 	private String description;
 	public Courses(long id, String title, String description) {
@@ -10,6 +10,9 @@ public class Courses {
 		this.title = title;
 		this.description = description;
 	}
+	// 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
+	
+	// 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
 	public Courses() {
 		super();
 		// TODO Auto-generated constructor stub
